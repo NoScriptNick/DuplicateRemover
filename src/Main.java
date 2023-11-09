8d55cee9f6c75198f048ab5b8b4d9eb3be7971c2
613668 <beres.613668@swl.k12.oh.us>
2023-11-09 09:36:26 -0500

    sdfg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        String s;
+        String current;
+        String newWord = "";
+
+        System.out.println("Enter a word: ");
+        s = scan.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            current = s.substring(i, i + 1);
+            if (i == 0 || !s.substring(0, i).contains(current)) {
+                newWord = newWord + current;
+            }
+        }
+        System.out.println(newWord);
     }
 }
